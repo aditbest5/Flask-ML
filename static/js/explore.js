1b67dbcb9199cc452c27b4d6051624d190b7d46f
aditbest5 <aditbest5@gmail.com>
2024-04-01 15:10:23 +0700

    test

---
+++ static/js/explore.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let imageUrl = "static/assets/PLTGU.jpg";
+  document.body.style.backgroundImage = `url(${imageUrl})`;
+  document.body.style.backgroundSize = "cover";
+});
